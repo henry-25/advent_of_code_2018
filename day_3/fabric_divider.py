0b1485baf8fbcb0cb10e46544a6c526ed1b08f9e
henry-25 <henry.r.welsh@gmail.com>
2018-12-03 16:59:19 -0500

    rules for find conflicts

--- day_3/fabric_divider.py
+++ day_3/fabric_divider.py
@@ -12,31 +12,34 @@ def main():
             line = line.split(':')
             upper_right = line[0].split(',') # upper_right[0] is dimension from left edge, upper_right[1] is dimension from top edge 
             width_height = line[1].split('x') # width_height[0] is width, width_height[1] is height
-            # Each corner is a tuple (x, y)
-            tmp['upper_left'] = {'x': int(upper_right[0]), 'y': int(upper_right[1])}
-            tmp['upper_right'] = {'x': int(upper_right[0]) + int(width_height[0]), 'y': int(upper_right[1])}
-            tmp['bottom_left'] = {'x': int(upper_right[0]), 'y': int(upper_right[1]) + int(width_height[1])}
-            tmp['bottom_right'] = {'x': int(upper_right[0]) + int(width_height[0]), 'y': int(upper_right[1]) + int(width_height[1])}
+            tmp['tlc'] = {'x': int(upper_right[0]), 'y': int(upper_right[1])}
+            tmp['width'] = int(width_height[0])
+            tmp['height'] = int(width_height[1])
+            # # Each corner is a tuple (x, y)
+            # tmp['upper_left'] = {'x': int(upper_right[0]), 'y': int(upper_right[1])}
+            # tmp['upper_right'] = {'x': int(upper_right[0]) + int(width_height[0]), 'y': int(upper_right[1])}
+            # tmp['bottom_left'] = {'x': int(upper_right[0]), 'y': int(upper_right[1]) + int(width_height[1])}
+            # tmp['bottom_right'] = {'x': int(upper_right[0]) + int(width_height[0]), 'y': int(upper_right[1]) + int(width_height[1])}
             claims_array.append(tmp)
 
-    for i, claim in enumerate(claims_array):
-        # print(i)
-        check_for_intersect(claim, claims_array[i:])
+    used_area_array.append(claims_array[0])
+    iterclaims = iter(claims_array)
+    next(iterclaims)
+    for claim in claims_array:
+        check_for_intersect(claim, used_area_array)
 
+def check_for_intersect(claim, used_area_array):
+    for aca in used_area_array:
+        if()
 
-#     used_area_array.append(claims_array[0])
-#     iterclaims = iter(claims_array)
-#     next(iterclaims)
-#     for claim in iterclaims:
-#         print('Newly claimed:', claim)
-#         check_for_intersect(claim, used_area_array)
-
-def check_for_intersect(claim, other_claims):
-    count = 0
-    for j, c in enumerate(other_claims):
-        print(j)
-        count += 1
-    print(count)
+# def check_for_intersect(claim, other_claims):
+#     for previously_used in other_claims:
+#         if(claim['upper_left']['x'] < previously_used['upper_left']['x'] and claim['upper_left']['y'] < previously_used['upper_left']['y']):
+#             if(claim['upper_right']['x'] > previously_used['upper_right']['x'] and claim['upper_right']['y'] < previously_used['upper_right']['y']):
+#                 if(claim['bottom_left']['x'] < previously_used['bottom_left']['x'] and claim['bottom_left']['y'] > previously_used['bottom_left']['y']):
+#                     if(claim['bottom_right']['x'] > previously_used['bottom_right']['x'] and claim['bottom_right']['y'] > previously_used['bottom_right']['y']):
+#                         print(claim, 'Contains', previously_used)
+    
 
 
 
